cf907a5614c62ac55350b67850a8a1d25e093998
tinjet <tinjet1016@gmail.com>
2023-09-14 16:28:35 +0800

    add skill text

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import Skills_card from './Skills_card'
-import { skill_content } from './constant'
+import { skill_content, skill_content_text } from './constant'
 
 
 const Skills = () => {
     return (
         <div className='mt-4 p-2'>
             <h1 className="text-left header-1 mb-3">Skills</h1>
+            <div className="flex flex-row flex-wrap mt-3">
+                {skill_content_text.map((tag, index) => (
+                    <div className="tag border-[#183f2c] py-1 px-2 rounded-full mr-2 mb-2" key={index}>
+                        {tag}
+                    </div>
+                ))}
+            </div>
             <div className='flex-col mb-2 pb-3'>
                 {
-                    skill_content.map((item,index) => (
+                    skill_content.map((item, index) => (
                         <Skills_card
-                        key={index}
+                            key={index}
                             title={item.title}
                             contents={item.contents}
                         />
                     ))
                 }
             </div>
+
+
         </div>
 
 
